93bceb6f76d381608842a83ffd9e1aa2bac207fc
Longquan Chen <leochan2009@hotmail.com>
2017-01-20 16:56:45 -0500

    FEATURE: Add Websocket in progress

---
+++ Source/igtlWebSocket.cxx
@@ -0,0 +1,41 @@
+//
+//  igtlWebSocket.cpp
+//  OpenIGTLink
+//
+//  Created by Longquan Chen on 1/20/17.
+//
+//
+
+#include "igtlWebSocket.h"
+
+
+#include <websocketpp/config/asio_no_tls.hpp>
+
+#include <websocketpp/server.hpp>
+
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <streambuf>
+#include <string>
+
+/**
+ * The telemetry server accepts connections and sends a message every second to
+ * each client containing an integer count. This example can be used as the
+ * basis for programs that expose a stream of telemetry data for logging,
+ * dashboards, etc.
+ *
+ * This example uses the timer based concurrency method and is self contained
+ * and singled threaded. Refer to telemetry client for an example of a similar
+ * telemetry setup using threads rather than timers.
+ *
+ * This example also includes an example simple HTTP server that serves a web
+ * dashboard displaying the count. This simple design is suitable for use
+ * delivering a small number of files to a small number of clients. It is ideal
+ * for cases like embedded dashboards that don't want the complexity of an extra
+ * HTTP server to serve static files.
+ *
+ * This design *will* fall over under high traffic or DoS conditions. In such
+ * cases you are much better off proxying to a real HTTP server for the http
+ * requests.
+ */
